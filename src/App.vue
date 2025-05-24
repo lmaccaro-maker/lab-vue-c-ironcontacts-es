7038898ae9d7c9d18b918bdc54914139728b6b6b
lmaccaro-maker <lauramaccarone91@gmail.com>
2025-05-24 09:32:08 +0200

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 // src/App.vue
 <template>
   <div class="app">
-    <h1>Iron Contacts</h1>
-    <ul>
-      <li v-for="contact in contacts" :key="contact.id">
-        <img
-          :src="contact.pictureUrl"
-          alt="Image de {{ contact.name }}"
-          class="profile"
-        />
-        <h2>{{ contact.name }}</h2>
-        <p>Popularity: {{ contact.popularity }}</p>
-      </li>
-    </ul>
+    <h1>IronContacts</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="contact in contacts" :key="contact.id">
+          <td><img :src="contact.pictureUrl" :alt="`Foto de ${contact.name}`" class="profile" /></td>
+          <td>{{ contact.name }}</td>
+          <td>{{ contact.popularity.toFixed(2) }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script setup>
 import contacts from "./contacts.json";
+import { ref } from 'vue'
+
 </script>
 
-<style></style>
+<style>
+ {
+  background-color:; 
+  font-weight: bold;
+  font-size:200px;
+  padding: 10px; 
+  
+}
+
+</style>
