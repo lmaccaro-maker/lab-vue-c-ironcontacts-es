0af39c922410da3c0e14d7db5a8160639edfe644
lmaccaro-maker <lauramaccarone91@gmail.com>
2025-05-24 10:15:21 +0200

    updated iteracion1-2

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,27 @@
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="contact in contacts" :key="contact.id">
-          <td><img :src="contact.pictureUrl" :alt="`Foto de ${contact.name}`" class="profile" /></td>
+          <td>
+            <img
+              :src="contact.pictureUrl"
+              :alt="`Foto de ${contact.name}`"
+              class="profile"
+            />
+          </td>
           <td>{{ contact.name }}</td>
           <td>{{ contact.popularity.toFixed(2) }}</td>
+          <td>
+            <span v-if="contact.wonOscar">🏆</span>
+          </td>
+          <td>
+            <span v-if="contact.wonEmmy">🏆</span>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -22,18 +36,15 @@
 </template>
 
 <script setup>
-import contacts from "./contacts.json";
-import { ref } from 'vue'
-
+import allContacts from "./contacts.json";
+import { ref } from "vue";
+const contacts = ref(allContacts.slice(5, 10));
 </script>
 
 <style>
  {
-  background-color:; 
   font-weight: bold;
-  font-size:200px;
-  padding: 10px; 
-  
+  font-size: 300px;
+  padding: 10px;
 }
-
 </style>
